d5db7602d6da0279565d35e60fa69e04bc99eb52
Karen Davis <1684687+karendavis@users.noreply.github.com>
2024-02-14 21:52:26 +1100

    Naive implementation of extracting text from screen shot of screentime data

--- data_loader/screen_time.py
+++ data_loader/screen_time.py
@@ -1,6 +1,24 @@
-import pandas as pd
+import cv2
+import pytesseract
+from dataclasses import dataclass
 
 
-def load_screen_time_data(folder) -> pd.DataFrame:
-    return None
+@dataclass
+class ScreenTimeItem:
+    date: str
+    total_time: str
+    applications: []
+
+
+def load_screen_time_data(img_file: str) -> ScreenTimeItem:
+    img = cv2.imread(img_file)
+    ref = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    ref = cv2.threshold(ref, 200, 200, 200)[1]
+    text = pytesseract.image_to_string(ref).split('\n')
+    screen_time_data = ScreenTimeItem(date=text[10], total_time=text[12],
+                                      applications={text[26]: text[27],
+                                                    text[29]: text[30],
+                                                    text[32]: text[34]})
+    return screen_time_data
+
 
--- tests/test_screen_time.py
+++ tests/test_screen_time.py
@@ -1,6 +1,16 @@
 from data_loader import screen_time
+from data_loader.screen_time import ScreenTimeItem
+
+mock_screen_time_data = ScreenTimeItem(date='Today, 13 February',
+                                       total_time='3h 19min.',
+                                       applications={
+                                            'Safari': '2h 49min.',
+                                            'Messages': '13min.',
+                                            'DuckDuckGo': '3min.'
+                                       })
 
 
 def test_load_screen_time_data():
-    data = screen_time.load_screen_time_data("test_data")
-    assert data is not None
+    result = screen_time.load_screen_time_data("test_data/13_Feb_2024_at_10.png")
+    assert result is not None
+    assert result == mock_screen_time_data
