e24c0b5f9b4b7b9fba0b03013c9ec5ddc7ccff08
Karen Davis <1684687+karendavis@users.noreply.github.com>
2024-02-18 16:14:51 +1100

    Making the screen time loader more robust, adding additional tests

--- data_loader/screen_time.py
+++ data_loader/screen_time.py
@@ -1,5 +1,6 @@
 import cv2
 import pytesseract
+import re
 from dataclasses import dataclass
 
 
@@ -10,15 +11,38 @@ class ScreenTimeItem:
     applications: []
 
 
+application_names = ["Safari", "Messages", "DuckDuckGo", "MyFitnessPal", "Connect", "The Lott", "Gmail"]
+
+
 def load_screen_time_data(img_file: str) -> ScreenTimeItem:
     img = cv2.imread(img_file)
     ref = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     ref = cv2.threshold(ref, 200, 200, 200)[1]
+
     text = pytesseract.image_to_string(ref).split('\n')
-    screen_time_data = ScreenTimeItem(date=text[10], total_time=text[12],
-                                      applications={text[26]: text[27],
-                                                    text[29]: text[30],
-                                                    text[32]: text[34]})
+    text = filter_empty_text_items(text)
+    screen_time_data = create_screen_time_item_from_text(text)
     return screen_time_data
 
 
+def filter_empty_text_items(text_items: list) -> list:
+    text_items = list(filter(None, text_items))
+    text_items = list(filter(lambda item: item.strip(), text_items))
+    # This Safari is not the one we want to use for application time
+    # text_items.remove('< Safari')
+    safari_regex = re.compile(r'<.*Safari')
+    text_items = [x for x in text_items if not safari_regex.match(x)]
+    return text_items
+
+
+def create_screen_time_item_from_text(text_items: list) -> list:
+    screen_time_date_idx = [i for i, item in enumerate(text_items) if 'Today,' in item][0]
+
+    application_indexes = [i for i, item in enumerate(text_items) if (any(name in item for name in application_names))]
+
+    applications = {}
+    for index in application_indexes:
+        applications[text_items[index]] = text_items[index+1]
+
+    return ScreenTimeItem(date=text_items[screen_time_date_idx], total_time=text_items[screen_time_date_idx + 1],
+                          applications=applications)
--- tests/test_screen_time.py
+++ tests/test_screen_time.py
@@ -9,8 +9,32 @@ mock_screen_time_data = ScreenTimeItem(date='Today, 13 February',
                                             'DuckDuckGo': '3min.'
                                        })
 
+mock_test_data = ['10:00', '. atl > (_', 'Week Day', 'SCREEN TIME',
+                  'Today, 16 February', '1 1 1 1 1 1 8h', '! — 0', 'M Tu Ww ‘Th F Sa Su', ', 60min.', '= 30min.',
+                  "'12am ‘6am '12pm ‘6pm", 'Health & Fitness Social Education', 'Smin. Smin. 2min.',
+                  'MOST USED SHOW CATEGORIES', 're) Safari', '1h 59min.', 'MyFitnessPal', '8min.', 'O Messages >',
+                  '8min.', 'yy SChO0 kiiytGae N']
+
 
 def test_load_screen_time_data():
     result = screen_time.load_screen_time_data("test_data/13_Feb_2024_at_10.png")
     assert result is not None
-    assert result == mock_screen_time_data
+    assert result.date == mock_screen_time_data.date
+    assert result.total_time == mock_screen_time_data.total_time
+    assert result.applications == mock_screen_time_data.applications
+
+
+def test_load_screen_time_data_with_different_file():
+    result = screen_time.load_screen_time_data("test_data/16_Feb_2024_at_10.png")
+    assert result is not None
+    assert result.date == 'Today, 16 February'
+    assert result.total_time == '1 1 1 1 1 1 8h'
+    assert result.applications == {'re) Safari': '1h 59min.', '. MyFitnessPal': '8min.', 'O Messages >': '8min.'}
+
+
+def test_create_screen_time_item_from_text():
+    result = screen_time.create_screen_time_item_from_text(mock_test_data)
+    assert result is not None
+    assert result.date == 'Today, 16 February'
+    assert result.total_time == '1 1 1 1 1 1 8h'
+    assert result.applications == {'re) Safari': '1h 59min.',  'MyFitnessPal': '8min.', 'O Messages >': '8min.'}
