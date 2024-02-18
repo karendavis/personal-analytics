d8f94869196c9dc3d5aa16f681a7029b5978ae5d
Karen Davis <1684687+karendavis@users.noreply.github.com>
2024-02-18 21:14:54 +1100

    Loading multiple screen time images from a folder and converting the text to a data frame

--- data_loader/screen_time.py
+++ data_loader/screen_time.py
@@ -1,20 +1,49 @@
 import cv2
+import pandas as pd
 import pytesseract
 import re
-from dataclasses import dataclass
+from dataclasses import dataclass, asdict
+import glob
+
+
+@dataclass
+class ApplicationItem:
+    application_name: str
+    application_time: str
+
+    def dict(self, index):
+        return {f'application_name_{index}': self.application_name, f'application_time_{index}': self.application_time}
 
 
 @dataclass
 class ScreenTimeItem:
     date: str
     total_time: str
-    applications: []
+    applications: [ApplicationItem]
+
+    def dict(self):
+        dictionary = {k: str(v) for k, v in asdict(self).items()}
+        dictionary.pop('applications')
+        for row in [application.dict(index) for index, application in enumerate(self.applications)]:
+            dictionary.update(row)
+        return dictionary
 
 
-application_names = ["Safari", "Messages", "DuckDuckGo", "MyFitnessPal", "Connect", "The Lott", "Gmail"]
+application_names = ["Safari", "Messages", "DuckDuckGo", "MyFitnessPal", "Connect", "Gmail"]
 
 
-def load_screen_time_data(img_file: str) -> ScreenTimeItem:
+def load_screen_time_data(folder: str) -> pd.DataFrame:
+    image_files = glob.glob(f"{folder}/*.png")
+    screen_time_data = []
+    for image_file in image_files:
+        screen_time_data.append(load_screen_time_item(image_file))
+
+    flattened_screen_data = [x.dict() for x in screen_time_data]
+    df = pd.DataFrame.from_dict(flattened_screen_data)
+    return df
+
+
+def load_screen_time_item(img_file: str) -> ScreenTimeItem:
     img = cv2.imread(img_file)
     ref = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     ref = cv2.threshold(ref, 200, 200, 200)[1]
@@ -28,8 +57,7 @@ def load_screen_time_data(img_file: str) -> ScreenTimeItem:
 def filter_empty_text_items(text_items: list) -> list:
     text_items = list(filter(None, text_items))
     text_items = list(filter(lambda item: item.strip(), text_items))
-    # This Safari is not the one we want to use for application time
-    # text_items.remove('< Safari')
+    # This Safari is not the one we want to use for getting the time spent on the application
     safari_regex = re.compile(r'<.*Safari')
     text_items = [x for x in text_items if not safari_regex.match(x)]
     return text_items
@@ -40,9 +68,9 @@ def create_screen_time_item_from_text(text_items: list) -> list:
 
     application_indexes = [i for i, item in enumerate(text_items) if (any(name in item for name in application_names))]
 
-    applications = {}
+    applications = []
     for index in application_indexes:
-        applications[text_items[index]] = text_items[index+1]
+        applications.append(ApplicationItem(application_name=text_items[index], application_time=text_items[index+1]))
 
     return ScreenTimeItem(date=text_items[screen_time_date_idx], total_time=text_items[screen_time_date_idx + 1],
                           applications=applications)
--- tests/test_screen_time.py
+++ tests/test_screen_time.py
@@ -1,40 +1,59 @@
-from data_loader import screen_time
-from data_loader.screen_time import ScreenTimeItem
+from data_loader.screen_time import ScreenTimeItem, ApplicationItem, load_screen_time_item, \
+    load_screen_time_data, create_screen_time_item_from_text
 
 mock_screen_time_data = ScreenTimeItem(date='Today, 13 February',
                                        total_time='3h 19min.',
-                                       applications={
-                                            'Safari': '2h 49min.',
-                                            'Messages': '13min.',
-                                            'DuckDuckGo': '3min.'
-                                       })
+                                       applications=[ApplicationItem(application_name='Safari',
+                                                                     application_time='2h 49min.'),
+                                                     ApplicationItem(application_name='Messages',
+                                                                     application_time='13min.'),
+                                                     ApplicationItem(application_name='DuckDuckGo',
+                                                                     application_time='3min.')]
+                                       )
+
+mock_screen_time_data_again = ScreenTimeItem(date='Today, 16 February',
+                                             total_time='1 1 1 1 1 1 8h',
+                                             applications=[ApplicationItem(application_name='re) Safari',
+                                                                           application_time='1h 59min.'),
+                                                           ApplicationItem(application_name='. MyFitnessPal',
+                                                                           application_time='8min.'),
+                                                           ApplicationItem(application_name='O Messages >',
+                                                                           application_time='8min.')]
+                                             )
 
 mock_test_data = ['10:00', '. atl > (_', 'Week Day', 'SCREEN TIME',
                   'Today, 16 February', '1 1 1 1 1 1 8h', '! — 0', 'M Tu Ww ‘Th F Sa Su', ', 60min.', '= 30min.',
                   "'12am ‘6am '12pm ‘6pm", 'Health & Fitness Social Education', 'Smin. Smin. 2min.',
-                  'MOST USED SHOW CATEGORIES', 're) Safari', '1h 59min.', 'MyFitnessPal', '8min.', 'O Messages >',
+                  'MOST USED SHOW CATEGORIES', 're) Safari', '1h 59min.', '. MyFitnessPal', '8min.', 'O Messages >',
                   '8min.', 'yy SChO0 kiiytGae N']
+
+folder_path = "test_data"
 
 
 def test_load_screen_time_data():
-    result = screen_time.load_screen_time_data("test_data/13_Feb_2024_at_10.png")
+    results = load_screen_time_data(folder_path)
+    assert results.shape == (2, 8)
+
+
+def test_load_screen_time_item():
+    result = load_screen_time_item(f"{folder_path}/13_Feb_2024_at_10.png")
     assert result is not None
     assert result.date == mock_screen_time_data.date
     assert result.total_time == mock_screen_time_data.total_time
     assert result.applications == mock_screen_time_data.applications
 
 
-def test_load_screen_time_data_with_different_file():
-    result = screen_time.load_screen_time_data("test_data/16_Feb_2024_at_10.png")
+def test_load_screen_time_item_with_different_file():
+    result = load_screen_time_item(f"{folder_path}/16_Feb_2024_at_10.png")
     assert result is not None
-    assert result.date == 'Today, 16 February'
-    assert result.total_time == '1 1 1 1 1 1 8h'
-    assert result.applications == {'re) Safari': '1h 59min.', '. MyFitnessPal': '8min.', 'O Messages >': '8min.'}
+    assert result.date == mock_screen_time_data_again.date
+    assert result.total_time == mock_screen_time_data_again.total_time
+    assert result.applications == mock_screen_time_data_again.applications
 
 
 def test_create_screen_time_item_from_text():
-    result = screen_time.create_screen_time_item_from_text(mock_test_data)
+    result = create_screen_time_item_from_text(mock_test_data)
     assert result is not None
-    assert result.date == 'Today, 16 February'
-    assert result.total_time == '1 1 1 1 1 1 8h'
-    assert result.applications == {'re) Safari': '1h 59min.',  'MyFitnessPal': '8min.', 'O Messages >': '8min.'}
+    assert result.date == mock_screen_time_data_again.date
+    assert result.total_time == mock_screen_time_data_again.total_time
+    assert result.applications == mock_screen_time_data_again.applications
